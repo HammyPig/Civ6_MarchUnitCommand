4e98f0e7c03a0856c54ace62c127303dd7a5dfcb
HammyPig <jamesbwalden@protonmail.com>
2022-12-24 17:39:45 +1100

    fix: did not show disabled icon and text

--- MarchUnitCommand/Scripts/MarchUnitCommand_UnitCommandDefs.lua
+++ MarchUnitCommand/Scripts/MarchUnitCommand_UnitCommandDefs.lua
@@ -13,7 +13,7 @@ MarchUnitCommand.DisabledToolTipString = Locale.Lookup("LOC_UNITCOMMAND_MARCH_DI
 MarchUnitCommand.VisibleInUI = true;
 
 function MarchUnitCommand.CanUse(pUnit : object)
-	return (pUnit:GetMovesRemaining() == pUnit:GetMaxMoves());
+	return	pUnit ~= nil;
 end
 
 function MarchUnitCommand.IsVisible(pUnit : object)
